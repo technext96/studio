b751bc889aa374c63e30aadbd4961327aa698368
tech next <technext96@gmail.com>
2025-08-05 05:15:08 +0000

    URL is not on Google This page is not indexed. Pages that aren't indexed

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'technext-six.vercel.app',
+          },
+        ],
+        destination: 'https://technext96.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
