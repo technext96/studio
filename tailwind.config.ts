c5c6cb7ff9831fd5a99543a41911a2c6de9e3e5c
tech next <technext96@gmail.com>
2025-08-07 12:29:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,8 @@ import {fontFamily} from 'tailwindcss/defaultTheme';
 const config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
