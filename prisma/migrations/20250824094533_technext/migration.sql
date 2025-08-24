f0289734c1bc141ef4d575d181a17068ff4b3fbd
tech next <technext96@gmail.com>
2025-08-24 09:52:55 +0000

    how i triger the process

---
+++ prisma/migrations/20250824094533_technext/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."Post";
+
+-- CreateTable
+CREATE TABLE "public"."Blog" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tags" TEXT[],
+    "seoTitle" TEXT NOT NULL,
+    "seoDescription" TEXT NOT NULL,
+    "seoKeywords" TEXT NOT NULL,
+    "jsonLd" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Blog_slug_key" ON "public"."Blog"("slug");
