148ebf61e43f6d3bab4289716b51421042efed84
tech next <technext96@gmail.com>
2025-08-25 10:49:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getIronSession } from 'iron-session/next';
+import { getIronSession } from 'iron-session/edge';
 import { SessionData, sessionOptions } from '@/lib/session';
 
 export async function middleware(request: NextRequest) {
