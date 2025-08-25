8c46ce87a0fbd9a2d6d59135e0af14970371d16b
tech next <technext96@gmail.com>
2025-08-25 07:02:49 +0000

    Message: Error in Prisma Client request:
    Invalid `STUDIO_EMBED_BUILD<

--- src/app/admin/blogs/page.tsx
+++ src/app/admin/blogs/page.tsx
@@ -1,3 +1,4 @@
+
 import { PrismaClient, Blog, BlogStatus } from '@/generated/prisma';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
